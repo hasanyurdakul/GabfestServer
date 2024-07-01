688e6b344fdfca40088bbaab70ef258e01b03115
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-07-02 02:40:15 +0300

    Update UserUpdateModel.cs

--- Gabfest.Services/Models/User/UserUpdateModel.cs
+++ Gabfest.Services/Models/User/UserUpdateModel.cs
@@ -3,11 +3,11 @@
 public record UserUpdateModel
 {
     public int Id { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
-    public string FullName { get; set; }
-    public string Avatar { get; set; }
-    public string Role { get; set; }
-    public string ConnectionId { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+    public string? FullName { get; set; }
+    public string? Avatar { get; set; }
+    public string? Role { get; set; }
+    public string? ConnectionId { get; set; }
 }
