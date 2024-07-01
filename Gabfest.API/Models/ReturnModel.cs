453e707180be2b2c167ae374b4d1bc461cc15e68
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-07-02 00:51:20 +0300

    Create ReturnModel.cs

---
+++ Gabfest.API/Models/ReturnModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Gabfest.API;
+
+public class ReturnModel
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public int StatusCode { get; set; }
+    public object Data { get; set; }
+}
