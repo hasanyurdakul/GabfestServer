d9abf0469426ee60f35481a819e07c8f40b79959
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-07-02 14:53:19 +0300

    Create AuthController.cs

---
+++ Gabfest.API/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Gabfest.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gabfest.API;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IUserService userService, IConfiguration configuration)
+    {
+        _userService = userService;
+        _configuration = configuration;
+    }
+
+    [HttpPost("Login")]
+    public async Task<ReturnModel> Login([FromBody] LoginModel loginModel)
+    {
+        var user = await _userService.GetUserByUsernameAndPasswordAsync(loginModel.Username, loginModel.Password);
+        if (user == null)
+        {
+            return new ReturnModel
+            {
+                Success = false,
+                Message = "Invalid username or password",
+                StatusCode = 400
+            };
+        }
+        var tokenModel = new TokenModel
+        {
+            Username = user.Username,
+            Role = user.Role,
+            SignInKey = _configuration["JWT:SignInKey"],
+            Issuer = _configuration["JWT:Issuer"],
+            Audience = _configuration["JWT:Audience"]
+        };
+        var token = JWTHelper.GenerateJWT(tokenModel);
+        return new ReturnModel
+        {
+            Success = true,
+            Message = "Login successful",
+            StatusCode = 200,
+            Data = token
+        };
+
+    }
+
+}
