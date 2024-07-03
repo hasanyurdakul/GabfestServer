adc84dabd1216d6d249755041d1da3a2109bbb66
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-07-03 12:50:58 +0300

    Create HubController.cs

---
+++ Gabfest.API/Controllers/HubController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Gabfest.API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HubController : ControllerBase
+{
+    private readonly ISignalRConnection _signalRConnection;
+
+    public HubController(ISignalRConnection signalRConnection)
+    {
+        _signalRConnection = signalRConnection;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] string message)
+    {
+        var connect = _signalRConnection.StartConnection();
+        await connect.InvokeAsync("SendMessageToAll", "Admin", message);
+        return Ok();
+    }
+}
